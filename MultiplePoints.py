2489e4a2c73781f27e11170928bbb3e333c69e83
Satarupa Kundu <satarupakundu16@gmail.com>
2025-06-22 18:20:38 +0530

    Create MultiplePoints.py

---
+++ MultiplePoints.py
@@ -0,0 +1,10 @@
+#code:3 Multiple Points
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 15, 30, 5, 50])
+ypoints = np.array([0, 100, 200, 300, 400])
+
+plt.plot(xpoints, ypoints)
+plt.show()
